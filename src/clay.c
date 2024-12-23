2938f1de7b7d907cd949ad0bc98a097c0390710b
Nimaoth <nicolas.otremba@web.de>
2024-12-23 22:05:44 +0100

    Added utility function for interning strings in temp arena

--- src/clay.c
+++ src/clay.c
@@ -1,2 +1,14 @@
 #define CLAY_IMPLEMENTATION
 #include "clay/clay.h"
+
+Clay_String Clay__InternString(Clay_String string) {
+    if (Clay__dynamicStringData.length + string.length > Clay__dynamicStringData.capacity)
+    {
+        return CLAY_STRING("OOM");
+    }
+
+    char *chars = (char *)(Clay__dynamicStringData.internalArray + Clay__dynamicStringData.length);
+    memcpy(chars, string.chars, string.length);
+    Clay__dynamicStringData.length += string.length;
+    return CLAY__INIT(Clay_String) { .length = string.length, .chars = chars };
+}
